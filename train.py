f099de34046aea2e690ffbae80ed8d5e4262b1c3
Manoj N <Manoj.1.N@NIIT-Tech.com>
2019-11-28 18:27:48 +0530

    risk analysis1

---
+++ train.py
@@ -0,0 +1,72 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from itertools import cycle
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import lasso_path, enet_path
+filePath = "PD_Commercial_Train1.csv"
+
+df_credit = pd.read_csv(filePath)
+
+cols = df_credit.columns
+data = df_credit[cols].apply(pd.to_numeric, errors='coerce')
+data = data.fillna(0)
+
+X = data.drop(["PD"], axis=1)
+y = data[["PD"]]
+
+# Import mlflow
+import mlflow
+import mlflow.sklearn
+
+
+# Evaluate metrics
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+    mlflow.set_experiment('experiment4')
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "progression" which is a quantitative measure of disease progression one year after baseline
+    train_x = train.drop(["PD"], axis=1)
+    test_x = test.drop(["PD"], axis=1)
+    train_y = train[["PD"]]
+    test_y = test[["PD"]]
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.05
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.05
+
+    # Run ElasticNet
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    # Print out ElasticNet model metrics
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    # Log mlflow attributes for mlflow UI
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    mlflow.sklearn.log_model(lr, "model")
+    
+   
